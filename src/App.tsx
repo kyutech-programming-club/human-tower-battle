1f3b73d64a370dcdc146994d67889bf0d78d9d86
mkdiryuko <mtahtorhcai54891@gmail.com>
2025-09-30 23:03:03 +0900

    人物セグメンテーションの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Game from "./components/GameCanvas";
+import BodyPixTest from "./components/BodyPix";
 
 const App: React.FC = () => {
   return (
     <div>
       <Game />
+      <div style={{ padding: 20, borderTop: "2px solid #ddd", marginTop: 20 }}>
+        <BodyPixTest />
+      </div>
     </div>
   );
 };
