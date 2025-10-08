d67ae73d64b37431ea0fe7613fae70bdab3e9a6a
森雄士郎 <130552004+yu371@users.noreply.github.com>
2025-10-08 13:27:38 +0900

    Update Stage1.tsx

--- src/stages/Stage1.tsx
+++ src/stages/Stage1.tsx
@@ -5,11 +5,11 @@ export function createStage1(
   ctx: CanvasRenderingContext2D
 ) {
   // 床の定義
-<!--   const groundWidth = 450;
+  const groundWidth = 450;
   const groundHeight = 20;
   const groundX = 225;
   const groundY = 530 - groundHeight / 2;
-  const ground = Matter.Bodies.rectangle(groundX, groundY, groundWidth, groundHeight, { isStatic: true }); -->
+  const ground = Matter.Bodies.rectangle(groundX, groundY, groundWidth, groundHeight, { isStatic: true });
 
 
   Matter.World.add(world, [ground]);
